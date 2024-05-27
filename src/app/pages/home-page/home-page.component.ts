0479fbd872e8c7820b1e978eefd95aa89af8bb46
ThaiNHSE173633 <thainhse173633@fpt.edu.vn>
2024-05-27 12:27:56 +0700

    init home page fe

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-
+  slides = [
+    {
+      image: 'assets/images/slider1.jpg',
+      hotelName: 'Bora Hotel',
+      heading: 'Reserve Room for Family Vacation'
+    },
+    {
+      image: 'assets/images/slider2.jpg',
+      hotelName: 'Deluxe Hotel',
+      heading: 'Make Your Vacation Comfortable'
+    },
+    {
+      image: 'assets/images/slider3.jpg',
+      hotelName: 'Luxe Hotel',
+      heading: 'A Best Place To Enjoy Your Life'
+    }
+  ];
 }
