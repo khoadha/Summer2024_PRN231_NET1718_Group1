04042dbc8a6d27d6c3231d919ee1778118782209
Khoa <khoadha9a2.dt@gmail.com>
2024-06-08 17:17:25 +0700

    update

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: false,
   baseUrl: 'https://localhost:7267/api/v1/',
   baseAuthUrl: 'https://localhost:7209/api/v1/',
+  odataUrl: 'https://localhost:7267/odata/',
 };
+
   
