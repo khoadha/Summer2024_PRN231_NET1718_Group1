baf3fd650a8550d06332d897b4f69f27a5b39181
Khoa <khoadha9a2.dt@gmail.com>
2024-07-06 01:25:36 +0700

    upd

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://hosteland-api.azurewebsites.net/api/v1/',
+    baseAuthUrl: 'https://hosteland-api.azurewebsites.net/api/v1/',
+    oDataUrl: 'https://hosteland-api.azurewebsites.net/api/v1/filter/',
+  };
+  
+    
